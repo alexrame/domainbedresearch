9ae7bd881f70fc1d3cc0a74b29ce007b80a2cb2e
Alexandre RAME <alexandre.rame.cl@gmail.com>
2022-03-31 14:49:39 +0200

    wip

--- domainbed/algorithms_inference.py
+++ domainbed/algorithms_inference.py
@@ -122,9 +122,10 @@ class Soup(algorithms.Ensembling):
         assert self._t_scaled
 
         if key in ["net", "soup"]:
-            return torch.mean(torch.stack(self._t_networks))
+            return torch.mean(torch.stack(self._t_networks)).view(-1)
         if key in ["swa", "soupswa"]:
-            return torch.mean(torch.stack(self._t_swas))
+            return torch.mean(torch.stack(self._t_swas)).view(-1)
+
         i = key[-1]
         if key == "net" + str(i):
             return self._t_networks[int(i)]
