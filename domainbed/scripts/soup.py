b1144916f7a6c4968d095b29fa7cec65a233ffb3
Alexandre RAME <alexandre.rame.cl@gmail.com>
2022-04-22 07:25:04 +0200

    wip

--- domainbed/scripts/soup.py
+++ domainbed/scripts/soup.py
@@ -505,8 +505,8 @@ def find_checkpoints(inf_args, verbose=False):
         if os.environ.get("STEPS"):
             step = run_results.get("step", 5000)
             if os.environ.get("STEPS").startswith("mod"):
-                if name_folder == "best":
-                    print("Skip best")
+                if name_folder in ["best", "0", "50", "100", "200", "300", "400"]:
+                    gpuprint(f"Skip {name_folder}")
                     continue
                 if int(step) % int(os.environ.get("STEPS")[3:]) != 0:
                     gpuprintv(f"bad step {step} for: {name_folder}", verbose)
